6875d123dd460962cc18daefb6c98663ac2e0aea
Umberto46x <theumberto46@gmail.com>
2025-06-05 11:02:19 +0200

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './Models/User';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
 
+    <pre>{{ users | json }}</pre>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'angular-dep-inj-rest-services';
+  http = inject(HttpClient);
+  users: User[] = [];
+
+  constructor() {
+    this.http
+      .get<User[]>(`https://jsonplaceholder.typicode.com/users`)
+      .subscribe((result) => {
+        this.users = result;
+      });
+  }
 }
